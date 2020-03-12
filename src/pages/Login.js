a1c2ec791d1b5e444163efe6b72bbcc83d77a328
valdir-ti <valdir.ti@gmail.com>
2020-03-12 14:49:46 -0300

    Login apenas utilizando o nome do usuário

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,43 @@
 import React, { Component } from "react";
 
+import twitterLogo from "../twitter.png";
+import "./Login.css";
+
 export default class Login extends Component {
+  state = {
+    username: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { username } = this.state;
+
+    if (!username.length) return;
+
+    localStorage.setItem("@goTwitter:username", username);
+
+    this.props.history.push("/timeline");
+  };
+
+  handleInputChange = e => {
+    this.setState({ username: e.target.value });
+  };
+
   render() {
-    return <h2>Login</h2>;
+    return (
+      <div>
+        <div className="login-wrapper">
+          <img src={twitterLogo} alt="Twitter Clone" />
+          <form onSubmit={this.handleSubmit}>
+            <input
+              value={this.state.username}
+              placeholder="Nome de usuário"
+              onChange={this.handleInputChange}
+            />
+            <button type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }

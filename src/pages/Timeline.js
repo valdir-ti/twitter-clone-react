70ad0a1c3e773aa1597b3ec0dc2704e8ccd5ca1e
valdir-ti <valdir.ti@gmail.com>
2020-03-13 16:52:13 -0300

    Listando os tweets da API e criando o componente de tweets, necessário verificar a questão do cors pois os dados ainda não vieram

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -4,8 +4,11 @@ import api from "../services/api";
 import twitterLogo from "../twitter.png";
 import "./Timeline.css";
 
+import Tweet from "../components/Tweet";
+
 export default class Timeline extends Component {
   state = {
+    tweets: [],
     newTweet: ""
   };
 
@@ -15,12 +18,16 @@ export default class Timeline extends Component {
 
   handleNewTweet = async e => {
     if (e.keyCode !== 13) return;
-
     const content = this.state.newTweet;
     const author = localStorage.getItem("@goTwitter:username");
     await api.post("tweets", { author, content });
     this.setState({ newTweet: "" });
   };
+
+  async componentDidMount() {
+    const response = await api.get("tweets");
+    this.setState({ tweets: response.data });
+  }
 
   render() {
     return (
@@ -34,6 +41,11 @@ export default class Timeline extends Component {
             placeholder="O que está acontecendo?"
           ></textarea>
         </form>
+        <ul className="tweet-list">
+          {this.state.tweets.map(tweet => (
+            <Tweet key={tweet._id} tweet={tweet} />
+          ))}
+        </ul>
       </div>
     );
   }
